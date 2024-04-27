8aaaa05145a6c6b79af758d1cf094eb8f3933997
chaimaezo <chaimaezolati000@gmail.com>
2024-04-27 13:12:30 +0100

    Initial

--- src/pages/VehicleDetails.jsx
+++ src/pages/VehicleDetails.jsx
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, {useEffect , useState} from 'react';
 import { useParams } from 'react-router-dom';
 import './vehicleDetails.css';
 
 function VehicleDetails() {
+  const [car, setCar] = useState({})
   const { id } = useParams();
+  const index = parseInt(id)-1
+
+  const fetchData=()=>{
+    fetch('http://localhost:3000/api/vehiclesData.json')
+    .then(res => res.json())
+    .then(data =>setCar(data[index]))
+    .catch(e=> console.log(e.message));
+  }
+  useEffect(()=>{
+    fetchData();
+  },[])
+
   return (
-    <div>VehicleDetails - ID :  {id}</div>
-  )
+    <div id="vehicle-details" className="page vehicle-details">
+      <img src={car.pageBgImg} alt="" className="img-fluid page-img"/>
+    </div>
+  );
 }
 
-export default VehicleDetails
+export default VehicleDetails;
